5a015dac162713aeef70b0336ef6f912025b724b
n-ando <Noriaki.Ando@gmail.com>
2023-03-08 12:00:00 +0900

    .

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+brew_cellar=$(brew --cellar)
+echo $brew_cellar
+
+formulas="
+    omniorb-ssl-py38
+    omniorb-ssl-py39
+    omniorb-ssl-py310
+    omniorb-ssl-py311
+    omniorbpy-py38
+    omniorbpy-py39
+    omniorbpy-py310
+    omniorbpy-py311"
+
+brew_install()
+{
+    for f in $formulas; do
+        echo brew install $f
+        brew install $f
+        brew unlink $f
+    done
+}
+
+brew_install
