98aa454629b497202a3496af3d3ed1419d7630ac
Noriaki Ando <n-ando@n-andoMBP16M4.local>
2024-02-16 12:00:00 +0900

    .

---
+++ omniorb-ssl-py314.rb
@@ -0,0 +1,76 @@
+#============================================================
+# omniORB formula for HomeBrew
+#
+# Author: Noriaki Ando <Noriaki.Ando@gmail.com>
+# GitHub: https://github.com/OpenRTM/homebrew-omniorb
+#
+# This is the formula for omniORBpy on python3.14
+# To use this formula/bottle, switch python 3.x to python 3.14.
+# $ brew unlink python3.x (unlink current python)
+# $ brew link python@3.14 omniorb-ssl-y310
+#============================================================
+class OmniorbSslPy314 < Formula
+  desc "IOR and naming service utilities for omniORB with SSL"
+  homepage "https://omniorb.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/omniorb/omniORB/omniORB-4.3.4/omniORB-4.3.4.tar.bz2"
+  sha256 "79720d415d23cd8da99287a4ef4da0aa1bd34d3e4c7b1530715600adc5ed3dc3"
+
+  license all_of: ["GPL-2.0-only", "LGPL-2.1-only"]
+
+  livecheck do
+    url :stable
+    regex(%r{url=.*?/omniORB[._-]v?(\d+(?:\.\d+)+(?:-\d+)?)\.t}i)
+  end
+
+#  bottle do
+#    root_url "https://github.com/OpenRTM/homebrew-omniorb/releases/download/4.3.4/"
+#    rebuild 1
+#    sha256 cellar: :any, ventura: "19b15e83346afa2b6e141c9a3dc3e7daba557dc87e1c78056fcab5914ba420dd"
+#    rebuild 1
+#    sha256 cellar: :any, arm64_sequoia: "dce5103398da0dfbd302a0f7242a1b43f68aa068e246e664320821df96ff8c68"
+#    rebuild 1
+#    sha256 cellar: :any, arm64_sonoma: "dce5103398da0dfbd302a0f7242a1b43f68aa068e246e664320821df96ff8c68"
+#    rebuild 1
+#    sha256 cellar: :any, arm64_ventura: "dce5103398da0dfbd302a0f7242a1b43f68aa068e246e664320821df96ff8c68"
+#  end
+
+  depends_on "pkg-config" => :build
+  depends_on "openssl@3"
+  depends_on "python@3.14"
+  uses_from_macos "zlib"
+
+  resource "bindings" do
+    url "https://downloads.sourceforge.net/project/omniorb/omniORBpy/omniORBpy-4.3.4/omniORBpy-4.3.4.tar.bz2"
+    sha256 "cb5717d412a101baf430f598cac7d69231884dae4372d8e2adf3ddeebc5f7ebb"
+  end
+
+  def install
+    odie "bindings resource needs to be updated" if version != resource("bindings").version
+
+    ENV["PYTHON"] = python3 = which("python3.14")
+    xy = Language::Python.major_minor_version python3
+    inreplace "configure",
+              /am_cv_python_version=`.*`/,
+              "am_cv_python_version='#{xy}'"
+    args = ["--with-openssl"]
+    args << "--enable-cfnetwork" if OS.mac?
+    system "./configure", *args, *std_configure_args
+    system "make", "-j", "4"
+    system "make", "install"
+
+    resource("bindings").stage do
+      inreplace "configure",
+                /am_cv_python_version=`.*`/,
+                "am_cv_python_version='#{xy}'"
+      system "./configure", *std_configure_args
+      ENV.deparallelize # omnipy.cc:392:44: error: use of undeclared identifier 'OMNIORBPY_DIST_DATE'
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "#{bin}/omniidl", "-h"
+    system "#{bin}/omniidl", "-bcxx", "-u"
+    system "#{bin}/omniidl", "-bpython", "-u"
+  end
+end
