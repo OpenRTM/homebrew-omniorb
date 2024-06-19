56f6504225c9373e44f329bb09fd7f7703afb4ff
n-ando <Noriaki.Ando@gmail.com>
2024-06-19 12:00:00 +0900

    .

--- omniorb-ssl-py311.rb
+++ omniorb-ssl-py311.rb
@@ -31,7 +31,8 @@ class OmniorbSslPy310 < Formula
   depends_on "pkg-config" => :build
   depends_on "openssl@3"
   depends_on "python@3.11"
-
+  uses_from_macos "zlib"
+  
   resource "bindings" do
     url "https://downloads.sourceforge.net/project/omniorb/omniORBpy/omniORBpy-4.3.2/omniORBpy-4.3.2.tar.bz2"
     sha256 "cb5717d412a101baf430f598cac7d69231884dae4372d8e2adf3ddeebc5f7ebb"
